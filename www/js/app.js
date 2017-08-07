3800820b1c54c6754b5f1ec5a83fffbe381104b3
byunghyunpark <openmind8735@gmail.com>
2017-08-07 18:05:29 +0900

    Add detail page

--- www/js/app.js
+++ www/js/app.js
@@ -11,7 +11,8 @@ angular.module('App', ['ionic'])
       url: '/rates',
       views: {
         'rates-tab': {
-          templateUrl: 'views/rates/rates.html'
+          templateUrl: 'views/rates/rates.html',
+          controller: 'RatesController'
         }
       }
     })
@@ -32,6 +33,16 @@ angular.module('App', ['ionic'])
           templateUrl: 'views/currencies/currencies.html'
         }
       }
+    })
+    
+    .state('tabs.detail', {
+      url: '/detail/:currency',
+      views: {
+        'rates-tab': {
+          templateUrl: 'views/detail/detail.html',
+          controller: 'DetailController'
+        }
+      }
     });
 
   $urlRouterProvider.otherwise('/tabs/rates');
@@ -47,3 +58,118 @@ angular.module('App', ['ionic'])
     }
   });
 })
+
+// .factory('Currencies', function() {
+//   return [
+//     {
+//       code: 'BTCAUD', 
+//       text: 'Australian Dollar', 
+//       selected: true
+//     },
+//     {
+//       code: 'BTCBRL', 
+//       text: 'Brazilian Real', 
+//       selected: false
+//     },
+//     {
+//       code: 'BTCCAD',
+//       text: 'Canadian Dollar',
+//       selected: true
+//     }, 
+//     {
+//       code: 'BTCCHF',
+//       text: 'Swiss Franc',
+//       selected: false
+//     },
+//     {
+//       code: 'BTCCNY',
+//       text: 'Chinese Yuan',
+//       selected: true
+//     }, 
+//     {
+//       code: 'BTCEUR',
+//       text: 'Euro',
+//       selected: true
+//     },
+//     {
+//       code: 'BTCGBP',
+//       text: 'British Pound Sterling',
+//       selected: true
+//     }, 
+//     {
+//       code: 'BTCIDR',
+//       text: 'Indonesian Rupiah',
+//       selected: false
+//     },
+//     {
+//       code: 'BTCILS',
+//       text: 'Israeli New Sheqel',
+//       selected: false
+//     }, 
+//     {
+//       code: 'BTCMXN',
+//       text: 'Mexican Peso',
+//       selected: true
+//     },
+//     {
+//       code: 'BTCNOK',
+//       text: 'Norwegian Krone',
+//       selected: false
+//     }, 
+//     {
+//       code: 'BTCNZD',
+//       text: 'New Zealand Dollar',
+//       selected: false
+//     },
+//     {
+//       code: 'BTCPLN',
+//       text: 'Polish Zloty',
+//       selected: false
+//     }, 
+//     {
+//       code: 'BTCRON',
+//       text: 'Romanian Leu',
+//       selected: false
+//     },
+//     {
+//       code: 'BTCRUB',
+//       text: 'Russian Ruble',
+//       selected: true
+//     },
+//     {
+//       code: 'BTCSEK',
+//       text: 'Swedish Krona',
+//       selected: false
+//     },
+//     {
+//       code: 'BTCSGD',
+//       text: 'Singapore Dollar',
+//       selected: false
+//     },
+//     {
+//       code: 'BTCUSD',
+//       text: 'United States Dollar',
+//       selected: true
+//     },
+//     {
+//       code: 'BTCZAR',
+//       text: 'South African Rand',
+//       selected: false
+//     }
+//   ];
+// });
+
+.factory('Currencies', function () {
+  return [
+    {
+      code: 'BTCUSD',
+      text: 'United States Dollar',
+      selected: true
+    },
+    {
+      code: 'BTCZAR',
+      text: 'South African Rand',
+      selected: true
+    }
+  ];
+});
---
+++ www/views/rates/rates.js
@@ -0,0 +1,16 @@
+angular.module('App')
+    .controller('RatesController', function ($scope, $http, Currencies) {
+
+        $scope.currencies = Currencies;
+
+        $scope.load = function () {
+            $http.get('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD').success(function (tickers) {
+                angular.forEach($scope.currencies, function (currency) {
+                    currency.ticker = tickers;
+                    currency.ticker.timestamp = new Date(currency.ticker.timestamp);
+                });
+            });
+        };
+
+        $scope.load();
+    });
